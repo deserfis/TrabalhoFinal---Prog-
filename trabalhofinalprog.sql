868cdbcab86b200265d2e6503f4a235072dd583e
dogeatsburrito <lucasgrassihasse@gmail.com>
2025-01-03 14:12:51 -0300

    Update trabalhofinalprog.sql

--- trabalhofinalprog.sql
+++ trabalhofinalprog.sql
@@ -17,7 +17,7 @@ CREATE TABLE filme (
     nome VARCHAR(30) NOT NULL,
     ano_lancamento YEAR NOT NULL,
     descricao TEXT NOT NULL,
-    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5), -- Ajustado para DECIMAL
+    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5),
     genero_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (genero_id) REFERENCES genero (id)
@@ -28,7 +28,7 @@ DEFAULT COLLATE utf8_general_ci;
 CREATE TABLE avaliacao (
     id INT NOT NULL AUTO_INCREMENT,
     usuario VARCHAR(30) NOT NULL,
-    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5), -- Ajustado para DECIMAL
+    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5),
     comentario VARCHAR(1000) NOT NULL,
     filme_id INT NOT NULL,
     PRIMARY KEY(id),
