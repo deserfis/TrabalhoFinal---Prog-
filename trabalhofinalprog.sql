5d7826cd7a66dd80b0bb48d14e7609cab0d7ab81
dogeatsburrito <lucasgrassihasse@gmail.com>
2025-01-03 14:12:30 -0300

    Update trabalhofinalprog.sql

--- trabalhofinalprog.sql
+++ trabalhofinalprog.sql
@@ -4,18 +4,6 @@ DEFAULT COLLATE utf8_general_ci;
 
 USE projetofinal;
 
-CREATE TABLE filmes (
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(30) NOT NULL,
-    diretor VARCHAR(30) NOT NULL DEFAULT 'Hayao Miyazaki',
-    ano_lancamento YEAR NOT NULL,
-    duracao INT NOT NULL CHECK (duracao > 0),
-    descricao TEXT NOT NULL,
-    PRIMARY KEY(id)
-)
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
-
 CREATE TABLE genero (
     id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
@@ -23,3 +11,29 @@ CREATE TABLE genero (
 )
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE filme (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    ano_lancamento YEAR NOT NULL,
+    descricao TEXT NOT NULL,
+    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5), -- Ajustado para DECIMAL
+    genero_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (genero_id) REFERENCES genero (id)
+)
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE avaliacao (
+    id INT NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(30) NOT NULL,
+    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5), -- Ajustado para DECIMAL
+    comentario VARCHAR(1000) NOT NULL,
+    filme_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(filme_id) REFERENCES filme(id)
+)
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
