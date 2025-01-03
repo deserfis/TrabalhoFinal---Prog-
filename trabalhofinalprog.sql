37e8d9ecaea679584509ec053b6d4d7d02b84dae
dogeatsburrito <lucasgrassihasse@gmail.com>
2025-01-02 23:36:43 -0300

    Update trabalhofinalprog.sql

--- trabalhofinalprog.sql
+++ trabalhofinalprog.sql
@@ -1,1 +1,17 @@
-create database projetofinal;
+CREATE DATABASE projetofinal
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+USE projetofinal;
+
+CREATE TABLE filmes (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    diretor VARCHAR(30) NOT NULL DEFAULT 'Hayao Miyazaki',
+    ano_lancamento YEAR NOT NULL,
+    duracao INT NOT NULL CHECK (duracao > 0),
+    descricao TEXT
+    PRIMARY KEY(id)
+)
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
