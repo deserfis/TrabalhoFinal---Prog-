0cacad9702cf049aef49c6b6ef2ed07c324be97a
dogeatsburrito <lucasgrassihasse@gmail.com>
2025-01-03 22:32:08 -0300

    Update trabalhofinalprog.sql

--- trabalhofinalprog.sql
+++ trabalhofinalprog.sql
@@ -25,29 +25,6 @@ CREATE DATABASE projetofinal
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
-USE projetofinal;
-
-CREATE TABLE genero (
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
-)
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
-
-CREATE TABLE filme (
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(30) NOT NULL,
-    ano_lancamento YEAR NOT NULL,
-    descricao TEXT NOT NULL,
-    nota DECIMAL(2,1) NOT NULL CHECK (nota BETWEEN 0.5 AND 5), 
-    genero_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (genero_id) REFERENCES genero (id)
-)
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
-
 CREATE TABLE avaliacao (
     id INT NOT NULL AUTO_INCREMENT,
     usuario VARCHAR(30) NOT NULL,
