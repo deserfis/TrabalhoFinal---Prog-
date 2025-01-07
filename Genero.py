69b5015f5f291facb6b1340905a90978f01bc6fe
deserfis <167900449+deserfis@users.noreply.github.com>
2025-01-07 00:32:02 -0300

    Update Genero.py

--- Genero.py
+++ Genero.py
@@ -14,12 +14,12 @@ class Genero:
 
     # Inserir um novo gênero no banco de dados
     def inserir(self, cursor):
-        if not self.pedir_senha():
+        if not self.pedir_senha(): # Requisita senha para o acesso
             print("Senha incorreta! Acesso negado.")
             return
         
         try:
-            query = "INSERT INTO generos (nome) VALUES (%s)"
+            query = "INSERT INTO genero (nome) VALUES (%s)" #Insere o genero na tabela
             cursor.execute(query, (self.__nome,))
             print(f"Gênero '{self.__nome}' inserido com sucesso!")
         except Exception as e:
@@ -27,51 +27,30 @@ class Genero:
 
     # Consultar filmes por gênero
     @staticmethod
-    def consultar_por_genero(cursor, genero_nome):
+    def consultar_por_genero(self, cursor):
         try:
-            genero_nome = genero_nome.upper()  # Convertendo para maiúsculo
-            query = "SELECT f.titulo FROM filmes f JOIN generos g ON f.genero = g.nome WHERE g.nome = %s"
-            cursor.execute(query, (genero_nome,))
+            query = "SELECT f.titulo FROM filme f JOIN genero g ON f.genero = g.nome WHERE g.nome = %s" #Seleciona o título dos filmes do respectivo gênero
+            cursor.execute(query, (self.__nome,))
             filmes = cursor.fetchall()
             if filmes:
-                print(f"Filmes do gênero '{genero_nome}':")
+                print(f"Filmes do gênero '{self.__nome}':") #Imprime o título dos filmes
                 for filme in filmes:
-                    print(f"- {filme[0]}")
+                    print(f"- {filme[0]} ")
             else:
-                print(f"Nenhum filme encontrado para o gênero '{genero_nome}'.")
+                print(f"Nenhum filme encontrado para o gênero '{self.__nome}'.")
         except Exception as e:
             print(f"Erro ao consultar filmes por gênero: {e}")
 
-    # Atualizar o nome de um gênero no banco de dados
-    def atualizar(self, cursor):
-        if not self.pedir_senha():
-            print("Senha incorreta! Acesso negado.")
-            return
-        
-        try:
-            novo_nome = input(f"Digite o novo nome para o gênero '{self.__nome}': ").strip().upper()
-            confirmacao = input(f"Você tem certeza que deseja alterar o nome do gênero para '{novo_nome}'? (s/n): ").strip().lower()
-            
-            if confirmacao == "s":
-                query = "UPDATE generos SET nome=%s WHERE nome=%s"
-                cursor.execute(query, (novo_nome, self.__nome))
-                self.__nome = novo_nome
-                print(f"Gênero atualizado para '{self.__nome}' com sucesso!")
-            else:
-                print("Alteração cancelada.")
-        except Exception as e:
-            print(f"Erro ao atualizar gênero: {e}")
-
     # Excluir um gênero do banco de dados
     def excluir(self, cursor):
-        if not self.pedir_senha():
+        if not self.pedir_senha(): # Requisita senha para o acesso
             print("Senha incorreta! Acesso negado.")
             return
         
         try:
             confirmacao = input(f"Você tem certeza que deseja excluir o gênero '{self.__nome}'? (s/n): ").strip().lower()
             if confirmacao == "s":
-                query = "DELETE FROM generos WHERE nome=%s"
+                query = "DELETE FROM genero WHERE nome=%s" #Deleta o gênero da tabela
                 cursor.execute(query, (self.__nome,))
                 print(f"Gênero '{self.__nome}' excluído com sucesso!")
             else:
